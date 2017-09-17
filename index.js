827af226b27ee97cf5cd42ebf90295c13a90f3f7
Anuj Gupta <anuj3918@gmail.com>
2017-09-17 21:13:55 +0530

    Set up server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", () => {
+	res.send({ Hi: "There" });
+});
+
+app.listen(PORT, () => {
+	console.log("server running on port : " + port);
+});
