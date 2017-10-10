cd18230825427e1ad7211f48ba54e63f2f3c1867
Anuj Gupta <anuj.gupta@bookmyshow.com>
2017-10-10 16:54:12 +0530

    Adding dev.js key file in dev branch

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '29514066604-1cgs9bh3gr022b4m9h15fpsflnrgrv5q.apps.googleusercontent.com',
+	googleClientSecret: 't2RBmzTCB8ZLvJz0UhtkP-t9',
+	mongoURI: 'mongodb://anuj3918:password@ds113455.mlab.com:13455/emaily-dev',
+	cookieKey: 'a123456789'
+};
