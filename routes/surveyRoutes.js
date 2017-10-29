37ae0bda02e54010435f1ea69ca0f217e268a976
Anuj Gupta <anuj3918@gmail.com>
2017-10-29 15:27:38 +0530

    Surveys rendering done on UI

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const isLoggedIn = require('./middlewares/isLoggedIn');
 const hasCredits = require('./middlewares/hasCredits');
 const mongoose = require('mongoose');
@@ -6,12 +9,53 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
-	app.get('/api/surveys/thanks', (req, res) => {
+
+	app.get('/api/surveys', isLoggedIn, async (req, res) => {
+		const surveys = await Survey.find({ _user: req.user.id })
+			.select({ recipients: false });
+
+		res.send(surveys);
+	});
+
+	app.post('/api/surveys/webhooks', (req, res) => {
+		const p = new Path('/api/surveys/:surveyId/:choice');
+
+		_.chain(req.body)
+			.map(({ email, url }) => {			
+				//	p.test() return null if it cannot find surveyId and choice 
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return {
+						email,
+						surveyId: match.surveyId,
+						choice: match.choice
+					}
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ surveyId, email, choice }) => {
+				Survey.updateOne({
+					_id: surveyId,
+					recipients: {
+						$elemMatch: { email: email, responded: false }
+					}
+				}, {
+					$inc: { [choice]: 1 },
+					$set: { 'recipients.$.responded': true },
+					lastResponded: new Date()
+				}).exec();
+			})
+			.value();
+
+		res.send({});
+	});
+
+	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('Thanks for voting!');
 	});
 
 	app.post('/api/surveys', isLoggedIn, hasCredits, async (req, res) => {
-
 		const { title, body, subject, recipients } = req.body;
 
 		const survey = new Survey({
@@ -22,7 +66,7 @@ module.exports = app => {
 				return { email: email.trim() };
 			}),
 			_user: req.user.id,
-			dateSent: Date.now()
+			dateSent: new Date()
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
