47752925a73f43b637f06e1d06b3f3fbe7411adc
Anuj Gupta <anuj3918@gmail.com>
2017-10-30 13:07:04 +0530

    Finishing up Landing component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,38 +6,45 @@ class Landing extends Component {
 			<div>
 				<div className="card blue-grey darken-1">
 					<div className="card-content white-text">
-						<span className="card-title">What is this app ?</span>
-						<p>Create your custom survey and send it out to the world. Ask any binary question and your your recipients will reply in Yes or No</p>
-						<p>Read below to send your first survey</p>
+						<span className="card-title">Ask any binary question and ask for opinion</span>
+						<p className="right">For queries contact: anuj3918@gmail.com</p>
+					</div>
+					<div className="card-action">
+						<ul className="collapsible" data-collapsible="accordion">
+							<li>
+								<div className="collapsible-header">
+									1.&nbsp;Login with Google
+								</div>
+							</li>
+							<li>
+								<div className="collapsible-header">
+									2.&nbsp;Add credits for free to your account
+								</div>
+							</li>
+							<li>
+								<div className="collapsible-header">
+									4.&nbsp;Use 4242-4242-4242-4242 as card number
+								</div>
+							</li>
+							<li>
+								<div className="collapsible-header">
+									5.&nbsp;Put a date in future as expiry and a 3 digit security code
+								</div>
+							</li>
+							<li>
+								<div className="collapsible-header">
+									6.&nbsp;Go to surveys page and send survey with a binary question
+								</div>
+							</li>
+							<li>
+								<div className="collapsible-header">
+									7.&nbsp;Come back to check your recipients responses
+								</div>
+							</li>
+						</ul>
 					</div>
 				</div>
-				<ul className="collapsible" data-collapsible="accordion">
-					<li>
-						<div className="collapsible-header">
-							1.&nbsp;Login with Google
-						</div>
-					</li>
-					<li>
-						<div className="collapsible-header">
-							2.&nbsp;Add credits for free to your account
-						</div>
-					</li>
-					<li>
-						<div className="collapsible-header">
-							4.&nbsp;Use 4242-4242-42424-4242 as card number
-						</div>
-					</li>
-					<li>
-						<div className="collapsible-header">
-							5.&nbsp;Put any future expiry date and 3 digit security code
-						</div>
-					</li>
-					<li>
-						<div className="collapsible-header">
-							6.&nbsp;Go to surveys page and send survey with a binary question
-						</div>
-					</li>
-				</ul>
+				
 			</div>
 		);
 	}
